589d50c40eda5c3c7e102e21a7939d949f39400b
Andres Rivero <andres.rivero@bupunku.com>
2023-09-01 09:10:47 -0400

    feat: add git blame & todo plugins, remove c from treesitter

--- lua/andres/lazy.lua
+++ lua/andres/lazy.lua
@@ -138,5 +138,15 @@ require("lazy").setup({
   config = function()
     require("scrollbar").setup()
   end
+},
+'f-person/git-blame.nvim',
+{
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
 }
 })
