e91d08e7e0a7997bb5957b541eabac18e2008294
Andres Rivero <andres19rivero@gmail.com>
2024-05-06 23:27:33 -0400

    enable background opacity

--- init.lua
+++ init.lua
@@ -15,3 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 require("andres")
 
 vim.cmd [[colorscheme gruvbox]]
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
