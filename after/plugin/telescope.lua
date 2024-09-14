3f7a04560dd98168506cc7d7042e177687dcd4cd
Andres Rivero <andres19rivero@gmail.com>
2024-09-14 10:03:49 -0400

    change telescope keybinding to search files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -38,9 +38,9 @@ require('telescope').setup({
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', function() builtin.find_files({ hidden = true, no_ignore = true }) end, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>f.', function() builtin.find_files({ cwd = utils.buffer_dir() }) end, {})
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>f.', function() builtin.find_files({ cwd = utils.buffer_dir() }) end, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ss', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
